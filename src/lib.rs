0b64faece00dc4c4a2a0a5ded22cb46c1e4f65a0
yinguobing <yingb@starj.ai>
2025-04-29 10:19:51 +0800

    add test cases

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,14 @@ pub mod msg {
     use serde::{Deserialize, Serialize};
 
     /// Time indicates a specific point in time, relative to a clock's 0 point.
+    /// # Examples
+    ///
+    /// ```
+    /// use ros2_builtin_interfaces::msg::Time;
+    /// let time = Time { sec: 0, nanosec: 1 };
+    /// assert_eq!(time.sec, 0);
+    /// assert_eq!(time.nanosec, 1);
+    /// ```
     #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Deserialize, Serialize)]
     pub struct Time {
         /// The seconds component, valid over all int32 values.
@@ -16,6 +24,14 @@ pub mod msg {
     }
 
     /// Duration defines a period between two time points. It is comprised of a seconds component and a nanoseconds component.
+    /// # Examples
+    ///
+    /// ```
+    /// use ros2_builtin_interfaces::msg::Duration;
+    /// let duration = Duration { sec: 0, nanosec: 1 };
+    /// assert_eq!(duration.sec, 0);
+    /// assert_eq!(duration.nanosec, 1);
+    /// ```
     #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Deserialize, Serialize)]
     pub struct Duration {
         /// Seconds component, range is valid over any possible int32 value.
@@ -23,5 +39,24 @@ pub mod msg {
 
         /// Nanoseconds component in the range of [0, 10e9).
         pub nanosec: u32,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_time() {
+        let time = msg::Time { sec: 0, nanosec: 1 };
+        assert_eq!(time.sec, 0);
+        assert_eq!(time.nanosec, 1);
+    }
+
+    #[test]
+    fn test_duration() {
+        let duration = msg::Duration { sec: 2, nanosec: 3 };
+        assert_eq!(duration.sec, 2);
+        assert_eq!(duration.nanosec, 3);
     }
 }
